11b52a0d3e10e318b9ee27a21a70c8c54002d5aa
Jacek Sieka <jacek@status.im>
2020-04-29 23:03:24 +0200

    x

--- build-in-docker.sh
+++ build-in-docker.sh
@@ -8,6 +8,6 @@ export PKG_CONFIG_PATH="$(echo /opt/qt/*/gcc_64/lib/pkgconfig)"
 export LD_LIBRARY_PATH="$(echo /opt/qt/*/gcc_64/lib/)"
 
 [ -e vendor/nimbus-build-system/makefiles ] || make V=1
-make clean
-make -j2 appimage
+make V=1 clean
+make V=1 -j2 appimage
 
