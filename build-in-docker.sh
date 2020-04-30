cd534f903e3dd59504cf699b21564be45eff73f4
Jacek Sieka <jacek@status.im>
2020-04-30 08:45:49 +0200

    x

--- build-in-docker.sh
+++ build-in-docker.sh
@@ -2,7 +2,7 @@
 
 sudo add-apt-repository ppa:git-core/ppa
 sudo apt-get update
-sudo apt-get install -y --fix-missing cmake build-essential git libpcre3-dev libssl-dev
+sudo apt-get install -y --fix-missing cmake build-essential git libpcre3-dev libssl-dev git
 
 # the minor Qt version keeps getting updated inside the Docker image
 export PKG_CONFIG_PATH="$(echo /opt/qt/*/gcc_64/lib/pkgconfig)"
